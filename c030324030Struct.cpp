4fb4780a11932a8cafd9d0b8a79b90bc1932b698
asfnnn <asadarifin28@gmail.com>
2025-06-01 18:53:00 +0800

    Penambahan pointer

--- c030324030Struct.cpp
+++ c030324030Struct.cpp
@@ -1,35 +1,35 @@
-#include "conio.h"
-#include "stdio.h"
-#include "string.h"
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-struct mahasiswa
-{
-    float ipk;
-    string nim;
-    string nama;
-    string alamat;
-    string jurusan;
-    string kelas;
+    struct mahasiswa{
+	string nim;
+	string nama;
+	string alamat;
+	float ipk;
 };
 
+int inputData(string *nim, string *nama, string *alamat, float *ipk) {
+    cout << "NIM = ";          getline(cin, *nim);
+    cout << "Nama = ";         getline(cin, *nama);
+    cout << "Alamat = ";       getline(cin, *alamat);
+    cout << "IPK = ";          cin >> *ipk;
+    cin.ignore();
+}
+
+void outputData(const mahasiswa &Asfn) {
+    system("cls");
+    cout << "\n------------- Data Pribadi ------------- :\n";
+    cout << "NIM    : " << Asfn.nim << endl;
+    cout << "Nama   : " << Asfn.nama << endl;
+    cout << "Alamat : " << Asfn.alamat << endl;
+    cout << "IPK    : " << Asfn.ipk << endl;
+    cout << "---------------------------------------\n";
+}
 
 int main() {
-    struct mahasiswa asd;
-    asd.ipk = 3.73;
-    asd.nim = "C030324030";
-    asd.nama = "asd";
-    asd.alamat = "Bunyamin";
-    asd.jurusan = "TI";
-    asd.kelas = "Kelas 2B";
-    
-
-    cout << "NIM: " << asd.nim << endl;
-    cout << "IPK: " << asd.ipk << endl;
-    cout << "Nama: " << asd.nama << endl;
-    cout << "Alamat: " << asd.alamat << endl;
-    cout << "Jurusan: " << asd.jurusan << endl;
-    cout << "Kelas: " << asd.kelas << endl;
+    mahasiswa Asfn;
+    inputData(&Asfn.nim, &Asfn.nama, &Asfn.alamat, &Asfn.ipk);
+    outputData(Asfn);
+    return 0;
 }
